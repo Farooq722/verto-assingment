87cea3f13a47e5948c18a61a372975e3f04a0a91
B FAROOQ <bfarooq2022@gmail.com>
2025-09-29 17:35:16 +0530

    final push ?

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,19 +1,28 @@
-import express from "express";
-import cors from "cors";
-import { empRouter } from "./routes";
+// import express from "express";
+// import cors from "cors";
+// import { empRouter } from "./routes";
 
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "*" }));
+// export const app = express();
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cors({ origin: "*" }));
+
+// const port = process.env.PORT || 8000;
+
+// app.get("/", (req, res) => {
+//   res.send("Backend is working!");
+// });
+
+// app.use("/api/employees", empRouter);
+
+// app.listen(port, () => {
+//   console.log(`Port is listening on ${port}`);
+// });
+
+
+import app from "./app";
 
 const port = process.env.PORT || 8000;
-
-app.get("/", (req, res) => {
-  res.send("Backend is working!");
-});
-
-app.use("/api/employees", empRouter);
 
 app.listen(port, () => {
   console.log(`Port is listening on ${port}`);
